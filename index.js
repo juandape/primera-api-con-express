1dfa33c1d31cf63687ab4a424e8fb5de07e5a25a
Juan David Peña <juandavidp76@hotmail.com>
2023-05-04 09:13:45 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+// const morgan = require("morgan");
+const morganBody = require("morgan-body");
+const bodyParser = require("body-parser");
+
+const persons = [
+  {
+    id: 1,
+    name: "Tony Stark",
+    number: "25-344567",
+  },
+  {
+    id: 2,
+    name: "Steve Rogers",
+    number: "24-334543",
+  },
+  {
+    id: 3,
+    name: "Natasha Romanoff",
+    number: "45-643489",
+  },
+  {
+    id: 4,
+    name: "Bruce Banner",
+    number: "76-904567",
+  },
+];
+
+const newPerson = {
+  id: Date.now(),
+  name: "Scott Lang",
+  number: "69-4323435",
+};
+
+const app = express();
+const port = 8080;
+const date = Date();
+const people = persons.length;
+
+app.use(bodyParser.json());
+app.use(bodyParser.json());
+morganBody(app);
+
+app.get("/api/persons", (req, res) => res.json(persons));
+app.get("/info", (req, res) =>
+  res.send(`Phonebook has info for ${people} people --  ${date}`)
+);
+app.get("/api/persons/:id", (req, res) => {
+  const { id } = req.params;
+  const person = persons.find((person) => person.id.toString() === id);
+  if (!person) {
+    return res.status(404).json({ message: "Person not found" });
+  }
+  return res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const { id } = req.params;
+  const person = persons.find((person) => person.id.toString() === id);
+  if (!person) {
+    return res.status(404).json({ message: "Person not found" });
+  }
+  return res.send(`Person has been delete`);
+});
+
+app.post("/api/persons", (req, res) => {
+  const newPersons = [...persons, newPerson];
+  // const { name, number } = req.params;
+  persons.map((person) => {
+    if (newPerson.name === person.name) {
+      return res.status(404).json({ message: "Person already exist" });
+    } else if (newPerson.number === "") {
+      return res.status(404).json({ message: "Number is empty" });
+    }
+  });
+  return res.json(newPersons);
+});
+
+// morgan.token("body", (req) => {
+//   return JSON.stringify(req.body);
+// });
+
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}!`)
+);
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Make It Real - My first Express API
+## This is a solution to the *Express API* project of the Make It Real course.
+
+## The challenge
+Create an API using Express
+
+## Screenshot
+![print screen](./assets/print-screen.png)
+## My process
+### Built with
+- Semantic markup using vscode editor
+- Express NodeJs using vs code
+- Postman
+- Chrome Browser
+
+## What I learned
+I learn and practice how to create an API using Express NodeJs
+
+## Author
+Juan David Peña  
+github - @juandape  
+email - juandavidp76@hotmail.com  
+
+## Acknowledgments
+Thanks to Make it Real to show me a path to become a better programmer
